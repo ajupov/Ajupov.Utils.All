d764bc00521e62edfe41236da8f209116c949b7b
Usman Ayupov <u.ayupov@dodopizza.com>
2020-04-19 20:45:36 +0300

    Update packages and change json serialize

--- Json/JsonExtensions.cs
+++ Json/JsonExtensions.cs
@@ -1,17 +1,23 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Ajupov.Utils.All.Json
 {
     public static class JsonExtensions
     {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
         public static T FromJsonString<T>(this string value)
         {
-            return JsonConvert.DeserializeObject<T>(value);
+            return JsonConvert.DeserializeObject<T>(value, Settings);
         }
 
         public static string ToJsonString(this object value)
         {
-            return JsonConvert.SerializeObject(value);
+            return JsonConvert.SerializeObject(value, Settings);
         }
     }
 }
