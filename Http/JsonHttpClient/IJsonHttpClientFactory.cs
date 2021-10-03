d81d0ffedb94a3c18c38d0312dd60b94330b54e5
Usman Ayupov <u.ayupov@dodopizza.com>
2021-10-03 21:27:22 +0300

    Fix

--- Http/JsonHttpClient/IJsonHttpClientFactory.cs
+++ Http/JsonHttpClient/IJsonHttpClientFactory.cs
@@ -18,13 +18,6 @@ namespace Ajupov.Utils.All.Http.JsonHttpClient
             Dictionary<string, string> headers = default,
             CancellationToken ct = default);
 
-        Task<T> PostAsync<T>(
-            string uri,
-            object parameters = default,
-            object body = default,
-            Dictionary<string, string> headers = default,
-            CancellationToken ct = default);
-
         Task PostAsync(
             string uri,
             object parameters = default,
@@ -32,7 +25,7 @@ namespace Ajupov.Utils.All.Http.JsonHttpClient
             Dictionary<string, string> headers = default,
             CancellationToken ct = default);
 
-        Task<T> PutAsync<T>(
+        Task<T> PostAsync<T>(
             string uri,
             object parameters = default,
             object body = default,
@@ -46,7 +39,7 @@ namespace Ajupov.Utils.All.Http.JsonHttpClient
             Dictionary<string, string> headers = default,
             CancellationToken ct = default);
 
-        Task<T> PatchAsync<T>(
+        Task<T> PutAsync<T>(
             string uri,
             object parameters = default,
             object body = default,
@@ -60,13 +53,20 @@ namespace Ajupov.Utils.All.Http.JsonHttpClient
             Dictionary<string, string> headers = default,
             CancellationToken ct = default);
 
-        Task<T> DeleteAsync<T>(
+        Task<T> PatchAsync<T>(
+            string uri,
+            object parameters = default,
+            object body = default,
+            Dictionary<string, string> headers = default,
+            CancellationToken ct = default);
+
+        Task DeleteAsync(
             string uri,
             object parameters = default,
             Dictionary<string, string> headers = default,
             CancellationToken ct = default);
 
-        Task DeleteAsync(
+        Task<T> DeleteAsync<T>(
             string uri,
             object parameters = default,
             Dictionary<string, string> headers = default,
